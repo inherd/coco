00f0ef44515ccfb8ff503d685c679667be4de544
Phodal Huang <h@phodal.com>
2021-01-13 14:20:29 +0800

    feat: add time format functsons

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,3 +1,27 @@
+extern crate chrono;
+use chrono::prelude::DateTime;
+use chrono::Utc;
+
+use std::time::{Duration, UNIX_EPOCH};
+
 pub mod cloud_native_identify;
 pub mod file_scanner;
 pub mod git_log_parser;
+
+pub fn format_unix_time(i: u64) -> String {
+    let d = UNIX_EPOCH + Duration::from_secs(i);
+    let datetime = DateTime::<Utc>::from(d);
+    let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
+    return timestamp_str;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::infrastructure::format_unix_time;
+
+    #[test]
+    fn format_commit_time() {
+        let time = format_unix_time(1610509414);
+        assert_eq!("2021-01-13 03:43:34.000000000", time);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,14 @@ pub mod infrastructure;
 
 #[cfg(test)]
 mod test {
-    use git2::{Error, Revspec};
     use std::path::PathBuf;
 
     #[test]
     fn should_open_repo() {
         use git2::Repository;
 
-        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        let repo = match Repository::open(path) {
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        match Repository::open(path) {
             Ok(repo) => {
                 let result = repo.revparse("master");
                 match result {
