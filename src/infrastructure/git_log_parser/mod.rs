a04353d90382168098cb3a846d3ffe1565ed1eed
Phodal Huang <h@phodal.com>
2021-01-13 14:56:18 +0800

    feat: make paths for testings

--- src/infrastructure/git_log_parser/mod.rs
+++ src/infrastructure/git_log_parser/mod.rs
@@ -1,1 +1,40 @@
+use git2::Repository;
+use std::path::PathBuf;
+use tempdir::TempDir;
+
 pub struct GitLogParser {}
+
+impl GitLogParser {
+    pub fn clone(url: &str) -> Repository {
+        let dir = PathBuf::from(".coco");
+        println!("tempdir: {:?}", dir.clone());
+        let repo = match Repository::clone(url, dir) {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to clone: {}", e),
+        };
+
+        return repo;
+    }
+
+    #[allow(dead_code)]
+    fn create_temp_dir() -> PathBuf {
+        let temp = TempDir::new_in(".coco", "");
+        let dir = match temp {
+            Ok(tempdir) => tempdir.into_path(),
+            Err(e) => panic!("failed to create dir: {}", e),
+        };
+        dir
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::infrastructure::git_log_parser::GitLogParser;
+
+    #[test]
+    fn should_support_clone() {
+        let repo = GitLogParser::clone("https://github.com/phodal/coco.fixtures");
+        let result = repo.revparse("master");
+        assert!(result.is_ok());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+extern crate tempdir;
+
 pub mod app;
 pub mod domain;
 pub mod infrastructure;
 
 #[cfg(test)]
 mod test {
+    use git2::Repository;
     use std::path::PathBuf;
 
     #[test]
     fn should_open_repo() {
-        use git2::Repository;
-
         let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         match Repository::open(path) {
             Ok(repo) => {
