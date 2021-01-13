ac5243eb1edc3d126ab433631cfafc8fb4633a37
Phodal Huang <h@phodal.com>
2021-01-13 16:43:14 +0800

    feat: make create dir by paths

--- src/infrastructure/git_log_parser/mod.rs
+++ src/infrastructure/git_log_parser/mod.rs
@@ -1,19 +1,54 @@
+use std::path::{Path, PathBuf};
+
 use git2::Repository;
-use std::path::PathBuf;
 use tempdir::TempDir;
+use url::Url;
 
 pub struct GitLogParser {}
 
 impl GitLogParser {
     pub fn clone(url: &str) -> Repository {
-        let dir = PathBuf::from(".coco");
-        println!("tempdir: {:?}", dir.clone());
-        let repo = match Repository::clone(url, dir) {
+        let root = Path::new(".coco");
+        let uri_path = match Url::parse(url) {
+            Ok(url) => url,
+            Err(e) => panic!("failed to parsed: {}", e),
+        };
+
+        let buf = GitLogParser::uri_to_path(root, uri_path);
+
+        println!("tempdir: {:?}", buf.clone());
+        if buf.exists() {
+            // todo: make update for repo
+            println!("todo: make update for repo");
+            let repo = match Repository::open(buf) {
+                Ok(repo) => repo,
+                Err(e) => panic!("failed to clone: {}", e),
+            };
+
+            return repo;
+        };
+
+        let repo = match Repository::clone(url, buf) {
             Ok(repo) => repo,
             Err(e) => panic!("failed to clone: {}", e),
         };
 
         return repo;
+    }
+
+    pub fn uri_to_path(root: &Path, uri_path: Url) -> PathBuf {
+        let mut buf = root.join(PathBuf::from(uri_path.host().unwrap().to_string()));
+
+        let paths = uri_path
+            .path_segments()
+            .map(|c| c.collect::<Vec<_>>())
+            .unwrap();
+
+        for path in paths {
+            buf = buf.join(PathBuf::from(path));
+        }
+
+        buf
     }
 
     #[allow(dead_code)]
