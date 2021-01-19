1df9b5ddb42245cc6984cc741e644d83bd01df95
Phodal Huang <h@phodal.com>
2021-01-19 09:19:00 +0800

    refactor: make naming in Rust style

--- src/infrastructure/git/git_branch.rs
+++ src/infrastructure/git/git_branch.rs
@@ -38,7 +38,7 @@ impl GitBranch {
             let oid = oid_result.unwrap();
             let commit = repo.find_commit(oid).unwrap();
 
-            commits.push(GitBranch::to_coco_commit(branch_name, oid, commit));
+            commits.push(GitBranch::convert_commit(branch_name, oid, commit));
         }
 
         branch.last_commit_date = commits[0].date;
@@ -55,7 +55,8 @@ impl GitBranch {
         (branch, commits)
     }
 
-    fn to_coco_commit(branch_name: &str, oid: Oid, commit: Commit) -> CocoCommit {
+    // todo: thinking in refactor to application, is not clean in infrastructure
+    fn convert_commit(branch_name: &str, oid: Oid, commit: Commit) -> CocoCommit {
         CocoCommit {
             branch: branch_name.to_string(),
             rev: oid.to_string(),
