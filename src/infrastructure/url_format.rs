fd689c4bb60a1752f4dcaea24a17bf4a4937fff6
Phodal Huang <h@phodal.com>
2021-01-19 20:56:48 +0800

    refactor: refactor old file dir logic

--- src/infrastructure/url_format.rs
+++ src/infrastructure/url_format.rs
@@ -5,13 +5,17 @@ use url::Url;
 use crate::settings::Settings;
 
 pub fn from(url: &str) -> String {
-    let url_str = url
-        .replace("https://", "")
-        .replace("http://", "")
-        .replace("/", "-")
-        .replace(".", "-");
+    let uri_path = match Url::parse(url) {
+        Ok(url) => url,
+        Err(e) => panic!("failed to parsed: {}", e),
+    };
 
-    return format!("{}.{}", url_str, "json");
+    let paths = uri_path
+        .path_segments()
+        .map(|c| c.collect::<Vec<_>>())
+        .unwrap();
+
+    return format!("{}.{}", paths.last().unwrap(), "json");
 }
 
 pub fn uri_to_path(url: &str) -> PathBuf {
@@ -40,21 +44,9 @@ mod test {
     use crate::infrastructure::url_format::{from, uri_to_path};
 
     #[test]
-    fn format_github() {
-        let string = from("github.com/coco-rs/coco.fixtures");
-        assert_eq!("github-com-coco-rs-coco-fixtures.json", string);
-    }
-
-    #[test]
-    fn format_github_with_url_https() {
-        let string = from("https://github.com/coco-rs/coco.fixtures");
-        assert_eq!("github-com-coco-rs-coco-fixtures.json", string);
-    }
-
-    #[test]
     fn format_github_with_url_http() {
         let string = from("http://github.com/coco-rs/coco.fixtures");
-        assert_eq!("github-com-coco-rs-coco-fixtures.json", string);
+        assert_eq!("coco.fixtures.json", string);
     }
 
     #[test]
