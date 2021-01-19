cf95f5aba1ec4d2263751bb35cfa47982dae32ab
Phodal Huang <h@phodal.com>
2021-01-19 15:38:53 +0800

    fix: fix serial error

--- src/domain/framework/coco_framework.rs
+++ src/domain/framework/coco_framework.rs
@@ -1,6 +1,6 @@
-use framework::framework_detector::Framework;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, PartialEq, Debug, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct CocoFramework {
     pub name: String,
     pub path: String,
@@ -13,16 +13,4 @@ pub struct CocoFramework {
     pub framework_files: Vec<String>,
     // in JVM projects, has different languages, such as Java, Groovy, Kotlin...
     pub languages: Vec<String>,
-}
-
-impl CocoFramework {
-    pub fn from(f: Framework) -> CocoFramework {
-        CocoFramework {
-            name: f.name,
-            path: f.path,
-            relative_path: f.relative_path,
-            framework_files: f.files,
-            languages: f.languages,
-        }
-    }
 }
