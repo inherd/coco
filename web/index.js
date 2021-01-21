83610948b1ebf67bf2d1aad341f7fde41f01bea4
Phodal Huang <h@phodal.com>
2021-01-21 20:13:55 +0800

    build: add sample code

--- web/index.js
+++ web/index.js
@@ -1,2 +1,15 @@
-d3.select("body").append("span")
-    .text("Hello, world!");
+function renderPacking(originData) {
+
+}
+
+d3.json("coco.json").then(function (json) {
+  let data = json;
+  for (let datum of json) {
+    if (datum.language === "Rust") {
+      data = datum;
+    }
+  }
+
+  renderPacking(data)
+});
+
