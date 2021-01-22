c8b76eb72e2d248cc6a4f478da641df8a222fcc3
Phodal Huang <h@phodal.com>
2021-01-22 11:07:29 +0800

    feat: init find_git_projects method

--- src/infrastructure/file_scanner/mod.rs
+++ src/infrastructure/file_scanner/mod.rs
@@ -1,3 +1,28 @@
-pub fn find_ignore_file() {}
+use std::path::Path;
+use walkdir::WalkDir;
 
-pub fn filter_by_ignore() {}
+pub fn find_git_projects<P: AsRef<Path>>(path: P) -> Vec<String> {
+    let mut results = vec![];
+    for entry in WalkDir::new(path).max_depth(1) {
+        let entry = entry.unwrap();
+        if entry.path().ends_with(".git") {
+            println!("{}", entry.path().display());
+            results.push(entry.path().display().to_string())
+        }
+    }
+
+    return results;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::infrastructure::file_scanner::find_git_projects;
+    use std::path::PathBuf;
+
+    #[test]
+    fn should_list_local_git() {
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let projects = find_git_projects(path);
+        assert_eq!(1, projects.len());
+    }
+}
