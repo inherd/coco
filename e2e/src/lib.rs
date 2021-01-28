b5e9137192010663dac25e65a4cc764218fdc0b7
Phodal Huang <h@phodal.com>
2021-01-28 13:39:51 +0800

    refactor: extract some e2e test files

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -1,5 +1,6 @@
-use std::env;
+use assert_cmd::Command;
 use std::path::PathBuf;
+use std::{env, fs};
 
 // Path to cargo executables
 pub fn cargo_dir() -> PathBuf {
@@ -21,15 +22,30 @@ pub fn coco_exe() -> PathBuf {
     cargo_dir().join(format!("coco{}", env::consts::EXE_SUFFIX))
 }
 
-pub fn coco_program() -> String {
-    return format!("{:?}", coco_exe());
+pub fn coco_cmd(path: PathBuf) -> Command {
+    let mut cmd = Command::cargo_bin("coco").unwrap();
+
+    cmd.arg("-c")
+        .arg(format!("{}", path.into_os_string().to_str().unwrap()));
+    cmd
+}
+
+pub fn output_file(typ: &str) -> PathBuf {
+    let mut output = PathBuf::from(".coco");
+    output.push("reporter");
+    output.push(typ);
+    output.push("coco.fixtures.json");
+    output
+}
+
+pub fn read_coco_reporter(typ: &str) -> String {
+    fs::read_to_string(output_file(typ)).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::coco_exe;
+    use crate::{coco_cmd, coco_exe, read_coco_reporter};
     use assert_cmd::Command;
-    use std::fs;
     use std::path::PathBuf;
 
     #[test]
@@ -47,19 +63,12 @@ mod tests {
     fn should_build_fixtures_code() {
         let mut path = PathBuf::from("_fixtures");
         path.push("coco-fixtures.yml");
-        let mut cmd = Command::cargo_bin("coco").unwrap();
-
-        cmd.arg("-c")
-            .arg(format!("{}", path.into_os_string().to_str().unwrap()));
-
+        let mut cmd = coco_cmd(path);
         cmd.assert().success();
 
-        let mut output = PathBuf::from(".coco");
-        output.push("reporter");
-        output.push("architecture");
-        output.push("coco.fixtures.json");
-
-        let result = fs::read_to_string(output).unwrap();
-        assert!(result.len() > 0);
+        assert!(read_coco_reporter("cloc").len() > 0);
+        assert!(read_coco_reporter("architecture").len() > 0);
+        assert!(read_coco_reporter("framework").len() > 0);
+        assert!(read_coco_reporter("git").len() > 0);
     }
 }
