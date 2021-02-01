37e78796602e8e6b64402d0e380e930478fe95e5
Phodal Huang <h@phodal.com>
2021-02-01 22:05:38 +0800

    docs: use short id for parse

--- src/infrastructure/git/git_command.rs
+++ src/infrastructure/git/git_command.rs
@@ -1,9 +1,10 @@
 use std::process::Command;
 
 pub fn get_commit_message() -> String {
-    // git log --pretty="format:[%H] %aN<%ae> %at (%P,%T) %s" --date=short --numstat --summary --date=unix --reverse
+    // git log --pretty="format:[%h] %aN<%ae> %at (%p,%t) %s" --date=short --numstat --summary --date=unix --reverse --branches
     let command = Command::new("git")
         .arg("log")
+        // more in: https://github.com/git/git/blob/master/Documentation/pretty-formats.txt
         // `%H`: commit hash
         // `%an`: author name
         // `%ae`: author email
@@ -11,7 +12,7 @@ pub fn get_commit_message() -> String {
         // `%P`: parent hashes
         // `%T`: tree hash
         // `%s`: subject
-        .arg("--pretty=\"format:[%H] %aN<%ae> %at (%P,%T) %s\"")
+        .arg("--pretty=\"format:[%h] %aN<%ae> %at (%p,%t) %s\"")
         .arg("--date=short")
         .arg("--numstat")
         .arg("--reverse")
