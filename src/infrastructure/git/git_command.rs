165e427f8a7b8c891e0ec50c45856e47637b1fd9
Phodal Huang <h@phodal.com>
2021-02-01 21:57:08 +0800

    docs: add info for log message

--- src/infrastructure/git/git_command.rs
+++ src/infrastructure/git/git_command.rs
@@ -1,11 +1,17 @@
 use std::process::Command;
 
 pub fn get_commit_message() -> String {
-    // git log --pretty="format:%H%n%aN%n%at%n%P%n%T%n--------%n%s" --date=short --numstat --summary --date=unix --reverse
+    // git log --pretty="format:[%H] %aN<%ae> %at (%P,%T) %s" --date=short --numstat --summary --date=unix --reverse
     let command = Command::new("git")
         .arg("log")
-        // -------- is the placeholder for parser object
-        .arg("--pretty=\"format:%H%n%aN%n%at%n%P%n%T%n--------%n%s\"")
+        // `%H`: commit hash
+        // `%an`: author name
+        // `%ae`: author email
+        // `%at`: author time
+        // `%P`: parent hashes
+        // `%T`: tree hash
+        // `%s`: subject
+        .arg("--pretty=\"format:[%H] %aN<%ae> %at (%P,%T) %s\"")
         .arg("--date=short")
         .arg("--numstat")
         .arg("--reverse")
