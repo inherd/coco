b0fe22667f3cabffe1551e73082eb29f702e65ed
Phodal Huang <h@phodal.com>
2021-02-04 16:02:41 +0800

    docs: init coco tag for match design

---
+++ src/domain/git/coco_tag.rs
@@ -0,0 +1,17 @@
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CocoTag {
+    pub name: String,
+    pub display_name: String,
+    pub date: i64
+}
+
+impl Default for CocoTag {
+    fn default() -> Self {
+        CocoTag {
+            name: "".to_string(),
+            display_name: "".to_string(),
+            date: 0
+        }
+    }
+}
