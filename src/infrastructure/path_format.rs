6cd9df947ec36c7f68747af3d7bf8780b4770ff1
Phodal Huang <h@phodal.com>
2021-02-04 14:51:11 +0800

    test: add lost test for expand

--- src/infrastructure/path_format.rs
+++ src/infrastructure/path_format.rs
@@ -1,5 +1,17 @@
 use std::path::Path;
 
 pub fn expand<P: AsRef<Path>>(p: P) -> String {
-    shellexpand::tilde(p).to_string()
+    shellexpand::tilde(&p.as_ref().to_str().unwrap()).to_string()
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::infrastructure::path_format::expand;
+
+    #[test]
+    fn format_path() {
+        let string = expand("~");
+        assert_ne!(string, "~");
+    }
 }
