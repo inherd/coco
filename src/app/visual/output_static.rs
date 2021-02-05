e71d3fcd77652c1abd0d599fdc603a5b01ef7587
Phodal Huang <h@phodal.com>
2021-02-05 09:48:33 +0800

    docs: add todo for output static

--- src/app/visual/output_static.rs
+++ src/app/visual/output_static.rs
@@ -1,17 +1,17 @@
 use rust_embed::RustEmbed;
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 
 #[derive(RustEmbed)]
 #[folder = "web/"]
 struct Asset;
 
-pub fn run(path_str: &str) {
-    let path = PathBuf::from(path_str);
+pub fn run<P: AsRef<Path>>(path: P) {
+    copy_reporter_to(&path.as_ref());
 
     for file in Asset::iter() {
         let file_name = format!("{}", file.as_ref());
-        let file_path = &path.join(file.as_ref());
+        let file_path = &path.as_ref().join(file.as_ref());
 
         let content = Asset::get(&file_name).unwrap();
         let _ = fs::create_dir_all(&file_path.parent().unwrap());
@@ -19,4 +19,8 @@ pub fn run(path_str: &str) {
         println!("write to file: {}", file_path.display());
         fs::write(file_path, content).expect("cannot write file");
     }
+}
+
+pub fn copy_reporter_to(_path: &Path) {
+    // todo: make reporter copy
 }
