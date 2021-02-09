1f2345bed4297f54820c950b647796bfb0543d73
Phodal Huang <h@phodal.com>
2021-02-09 16:42:18 +0800

    docs(suggest): add basic detect explain for projects

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -4,3 +4,4 @@ pub mod code;
 pub mod config;
 pub mod framework;
 pub mod git;
+pub mod suggest;
---
+++ src/domain/suggest/mod.rs
@@ -0,0 +1,17 @@
+/// count git tag interval for insight of release
+/// also same to publish interval
+pub fn git_tag_interval() {}
+
+/// find multiple long branches working in process
+/// it will show the continuous delivery issue
+pub fn long_branch_count() {}
+
+/// show the data of weekend works' hours
+/// it will show the detail of hours
+pub fn weekend_work() {}
+
+/// show the average team members commit time
+/// frequently member's change means project's not stable for business project
+/// more members stay in a long time, will help project stable.
+/// unstable member's change need more Rules
+pub fn average_members_commit_time() {}
