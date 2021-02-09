658b655f414c8851d4f752d6e57097b309b9a852
Phodal Huang <h@phodal.com>
2021-02-09 10:13:53 +0800

    feat: add parse for tag name

--- src/infrastructure/git/git_tag_parser.rs
+++ src/infrastructure/git/git_tag_parser.rs
@@ -41,6 +41,11 @@ impl GitLogParser {
             let date = date_str.parse::<i64>().unwrap();
 
             for tag in tags {
+                if !tag.contains("tag:") {
+                    continue;
+                }
+
+                let tag = tag.split("tag: ").last().unwrap();
                 self.tags.push(CocoTag {
                     name: tag.to_string(),
                     display_name: tag.to_string(),
@@ -66,5 +71,23 @@ mod test {
         assert_eq!(1, tags.len());
         assert_eq!("92fffa9b", &tags[0].commit_id);
         assert_eq!(1571521692, tags[0].date);
+        assert_eq!("v0.21.0", tags[0].name);
+    }
+
+    #[test]
+    pub fn should_not_parse_branch() {
+        let input = "817b444 1611642635  (origin/add-license-1)";
+        let tags = GitLogParser::parse(input);
+
+        assert_eq!(0, tags.len());
+    }
+
+    #[test]
+    pub fn should_parse_multiple_tags() {
+        let input = "0f152d07 1582212561  (tag: v0.34.0, tag: std/0.34.0)";
+        let tags = GitLogParser::parse(input);
+
+        assert_eq!(2, tags.len());
+        assert_eq!("std/0.34.0", tags[1].name);
     }
 }
