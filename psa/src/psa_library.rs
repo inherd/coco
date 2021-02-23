8bca440795a43e2793d3721c3520da09249cbd7d
ynfeng <fynwin@gmail.com>
2021-02-23 15:00:42 +0800

    feat: add some struct of psa.

--- psa/src/psa_library.rs
+++ psa/src/psa_library.rs
@@ -1,1 +1,32 @@
-struct Library {}
+pub struct Library {
+    pub name: String,
+    pub group: String,
+    pub version: String,
+    pub scope: LibraryScope,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum LibraryScope {
+    Test,
+    Compile,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::psa_library::{Library, LibraryScope};
+
+    #[test]
+    fn should_create_library() {
+        let lib = Library {
+            group: "org.springframework.boot".to_string(),
+            name: "spring-boot-starter-web".to_string(),
+            version: "1.0.0-RELEASE".to_string(),
+            scope: LibraryScope::Compile,
+        };
+
+        assert_eq!(lib.group, "org.springframework.boot".to_string());
+        assert_eq!(lib.name, "spring-boot-starter-web".to_string());
+        assert_eq!(lib.version, "1.0.0-RELEASE".to_string());
+        assert_eq!(lib.scope, LibraryScope::Compile);
+    }
+}
--- psa/src/psa_module.rs
+++ psa/src/psa_module.rs
@@ -1,1 +1,46 @@
-pub struct Module {}
+use crate::psa_facet::Facet;
+
+pub struct Module {
+    pub name: String,
+    pub path: String,
+    pub facets: Vec<Facet>,
+}
+
+impl Module {
+    pub fn add_facet(&mut self, facet: Facet) {
+        self.facets.push(facet);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::psa_facet::Facet;
+    use crate::psa_module::Module;
+
+    #[test]
+    fn should_create_module() {
+        let module = Module {
+            name: "foo".to_string(),
+            path: "test/path".to_string(),
+            facets: vec![],
+        };
+
+        assert_eq!(module.name, "foo".to_string());
+        assert_eq!(module.path, "test/path".to_string());
+    }
+
+    #[test]
+    fn should_add_facet() {
+        let mut module = Module {
+            name: "foo".to_string(),
+            path: "test/path".to_string(),
+            facets: vec![],
+        };
+
+        module.add_facet(Facet {
+            name: "Java".to_string(),
+        });
+
+        assert_eq!(module.facets.len(), 1);
+    }
+}
