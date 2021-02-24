87474e8fa30c6f2baf7f5decffc304fccd320c07
Phodal Huang <h@phodal.com>
2021-02-24 10:43:24 +0800

    fix: fix typo

--- src/infrastructure/git/git_repository.rs
+++ src/infrastructure/git/git_repository.rs
@@ -21,13 +21,13 @@ impl GitRepository {
         };
 
         let _ = fs::create_dir_all(&local_path.parent().unwrap());
-        let repo = match Repository::clone(url, local_path) {
+        let repo = match Repository::clone(url, &local_path) {
             Ok(repo) => repo,
             Err(e) => {
                 let exist_string = "exists and is not an empty directory";
 
                 if e.to_string().contains(exist_string) {
-                    return match Repository::open(&local_path) {
+                    return match Repository::open(local_path) {
                         Ok(repo) => repo,
                         Err(e) => panic!("failed to open: {}", e),
                     };
