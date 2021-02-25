61115d090dc5472887e78b3151744f5b5270010f
Elliot <hack00mind@gmail.com>
2021-02-25 23:08:19 +0800

    feat(coco): add subcommand for downloading and unzip plugins

--- src/bin/coco.rs
+++ src/bin/coco.rs
@@ -1,7 +1,14 @@
 use std::fs::OpenOptions;
-use std::{env, io::Write};
+use std::{
+    env,
+    io::{Cursor, Read, Write},
+    process::exit,
+};
+use std::{fs, io, path::Path};
 
 use clap::{App, Arg};
+use reqwest;
+use zip;
 
 use coco::app::analysis;
 use coco::app::cmd::CocoCliOption;
@@ -29,11 +36,22 @@ fn main() {
                 .author("Inherd Group")
                 .about("A DevOps Efficiency Analysis and Auto-suggestion Tool."),
         )
+        .subcommand(
+            App::new("plugins")
+                .version(VERSION)
+                .author("Inherd Group")
+                .about("A DevOps Efficiency Analysis and Auto-suggestion Tool."),
+        )
         .get_matches();
 
     if matches.is_present("init") {
         create_config_file();
-        std::process::exit(0);
+        exit(0);
+    }
+
+    if matches.is_present("plugins") {
+        setup_plugins();
+        exit(0);
     }
 
     let config_file = matches.value_of("config").unwrap_or("coco.yml");
@@ -68,6 +86,90 @@ fn create_config_file() {
     {
         Ok(_) => println!("success created"),
         Err(e) => println!("coco.yml create faild: {}", e),
+    }
+}
+
+fn setup_plugins() {
+    let plugins_path = Path::new("coco_plugins");
+    create_plugin_dir(&plugins_path);
+
+    let reader = download_plugins();
+
+    unzip_plugins(reader, plugins_path)
+}
+
+fn create_plugin_dir(path_name: &Path) {
+    if path_name.exists() {
+        return ();
+    }
+    match fs::create_dir(&path_name) {
+        Ok(_) => println!("create plugin dir success"),
+        Err(e) => {
+            eprintln!("create plugin dir failed: {}", e);
+            exit(1);
+        }
+    }
+}
+
+fn download_plugins() -> Cursor<Vec<u8>> {
+    let target = format!(
+        "https://github.com/inherd/coco/releases/download/v{}/coco_plugins_{}.zip",
+        VERSION,
+        env::consts::OS
+    );
+    println!("download from {}", target);
+
+    let mut response = reqwest::blocking::get(&target).expect("download error");
+    let mut buf: Vec<u8> = vec![];
+    response.read_to_end(&mut buf).unwrap();
+    Cursor::new(buf)
+}
+
+fn unzip_plugins(reader: Cursor<Vec<u8>>, plugins_path: &Path) {
+    let mut archive = zip::ZipArchive::new(reader).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let out_path = match file.enclosed_name() {
+            Some(path) => plugins_path.join(path),
+            None => continue,
+        };
+
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("Plugin {} comment: {}", i, comment);
+            }
+        }
+
+        if (&*file.name()).ends_with('/') {
+            println!("File {} extracted to \"{}\"", i, out_path.display());
+            fs::create_dir_all(&out_path).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                out_path.display(),
+                file.size()
+            );
+            if let Some(p) = out_path.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&out_path).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&out_path, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
     }
 }
 
