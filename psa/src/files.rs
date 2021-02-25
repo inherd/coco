9b2014129b28f14463f2dbc5178013db83bf817e
ynfeng <fynwin@gmail.com>
2021-02-25 20:01:21 +0800

    feat(psa): module content root detect.

--- psa/src/files.rs
+++ psa/src/files.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
+use std::path::PathBuf;
 
+use std::collections::HashSet;
 use walkdir::WalkDir;
 
 pub fn list_file_names<P: AsRef<Path>>(path: P) -> Vec<String> {
@@ -16,6 +18,23 @@ pub fn list_file_names<P: AsRef<Path>>(path: P) -> Vec<String> {
         }
     }
     files
+}
+
+pub fn list_all<P: AsRef<Path>>(path: P) -> HashSet<String> {
+    let mut dirs = HashSet::new();
+    let walk_dir = WalkDir::new(path);
+    for dir_entry in walk_dir
+        .min_depth(1)
+        .sort_by(|a, b| a.file_name().cmp(b.file_name()))
+        .into_iter()
+    {
+        if dir_entry.is_err() {
+            panic!("{}", dir_entry.err().unwrap());
+        }
+
+        dirs.insert(dir_entry.unwrap().path().display().to_string());
+    }
+    dirs
 }
 
 pub fn list_sub_dirs<P: AsRef<Path>>(path: P) -> Vec<String> {
@@ -37,4 +56,24 @@ pub fn list_sub_dirs<P: AsRef<Path>>(path: P) -> Vec<String> {
         }
     }
     dirs
+}
+
+pub fn find_in_path(root_path: &str, file: Vec<&str>) -> Option<String> {
+    let all_files = list_all(root_path);
+    let mut parent_path = PathBuf::from(root_path).to_path_buf();
+    for each_part in file.into_iter() {
+        parent_path.push(each_part);
+    }
+    match all_files.contains(&parent_path.display().to_string()) {
+        true => Some(parent_path.display().to_string()),
+        _ => None,
+    }
+}
+
+pub fn join_path(root_path: &str, file: Vec<&str>) -> String {
+    let mut parent_path = PathBuf::from(root_path).to_path_buf();
+    for each_part in file.into_iter() {
+        parent_path.push(each_part);
+    }
+    parent_path.display().to_string()
 }
--- psa/src/jvm/maven_module.rs
+++ psa/src/jvm/maven_module.rs
@@ -1,12 +1,17 @@
 use std::path::Path;
 
-use crate::files::{list_file_names, list_sub_dirs};
+use crate::files::{find_in_path, list_file_names, list_sub_dirs};
 use crate::jvm::psa_jvm::ModuleAnalyzer;
 use crate::{Module, Project};
 
 pub struct MavenModuleAnalyzer {}
 
 impl MavenModuleAnalyzer {
+    fn detect_sub_modules(&self, module_path: &str, module: &mut Option<Module>) {
+        let sub_modules = &mut self.analysis_sub_modules(module_path);
+        module.as_mut().unwrap().add_sub_modules(sub_modules);
+    }
+
     fn analysis_sub_modules(&self, module_path: &str) -> Vec<Module> {
         let mut sub_modules = Vec::new();
         let sub_dirs = list_sub_dirs(Path::new(module_path));
@@ -19,14 +24,63 @@ impl MavenModuleAnalyzer {
         }
         sub_modules
     }
+
+    fn detect_content_root(&self, module_path: &str, mut module: &mut Option<Module>) {
+        self.detect_source_root(module_path, &mut module);
+        self.detect_resource_root(module_path, &mut module);
+        self.detect_test_source_root(module_path, &mut module);
+        self.detect_test_resource_root(module_path, &mut module);
+    }
+
+    fn detect_source_root(&self, module_path: &str, module: &mut Option<Module>) {
+        let path = module_path;
+        let source_root = find_in_path(path, vec!["src", "main", "java"]);
+        match source_root {
+            Some(source_root) => module.as_mut().unwrap().add_source_root(source_root),
+            _ => (),
+        }
+    }
+
+    fn detect_resource_root(&self, module_path: &str, module: &mut Option<Module>) {
+        let path = module_path;
+        let resource_root = find_in_path(path, vec!["src", "main", "resources"]);
+        match resource_root {
+            Some(resource_root) => module.as_mut().unwrap().add_resource_root(resource_root),
+            _ => (),
+        }
+    }
+
+    fn detect_test_source_root(&self, module_path: &str, module: &mut Option<Module>) {
+        let path = module_path;
+        let test_source_root = find_in_path(path, vec!["src", "test", "java"]);
+        match test_source_root {
+            Some(test_source_root) => module
+                .as_mut()
+                .unwrap()
+                .add_test_source_root(test_source_root),
+            _ => (),
+        }
+    }
+
+    fn detect_test_resource_root(&self, module_path: &str, module: &mut Option<Module>) {
+        let path = module_path;
+        let test_resource_root = find_in_path(path, vec!["src", "test", "resources"]);
+        match test_resource_root {
+            Some(test_resource_root) => module
+                .as_mut()
+                .unwrap()
+                .add_test_resource_root(test_resource_root),
+            _ => (),
+        }
+    }
 }
 
 impl ModuleAnalyzer for MavenModuleAnalyzer {
     fn analysis(&self, module_path: &str) -> Option<Module> {
         let mut module = create_module(module_path);
         if !module.is_none() {
-            let sub_modules = &mut self.analysis_sub_modules(module_path);
-            module.as_mut().unwrap().add_sub_modules(sub_modules);
+            self.detect_sub_modules(&module_path, &mut module);
+            self.detect_content_root(module_path, &mut module);
         }
         module
     }
