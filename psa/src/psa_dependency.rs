7bd981198c7fe3a1fa36508e9b838100484a9d2f
ynfeng <fynwin@gmail.com>
2021-02-27 14:16:58 +0800

    refactor(psa): rename Library -> Dependency.

--- psa/src/psa_dependency.rs
+++ psa/src/psa_dependency.rs
@@ -1,33 +1,33 @@
 #[derive(Serialize)]
-pub struct Library {
+pub struct Dependency {
     pub name: String,
     pub group: String,
     pub version: String,
-    pub scope: LibraryScope,
+    pub scope: DependencyScope,
 }
 
 #[derive(Serialize, PartialEq, Debug)]
-pub enum LibraryScope {
+pub enum DependencyScope {
     Test,
     Compile,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{Library, LibraryScope};
+    use crate::{Dependency, DependencyScope};
 
     #[test]
-    fn should_create_library() {
-        let lib = Library {
+    fn should_create_dependency() {
+        let lib = Dependency {
             group: "org.springframework.boot".to_string(),
             name: "spring-boot-starter-web".to_string(),
             version: "1.0.0-RELEASE".to_string(),
-            scope: LibraryScope::Compile,
+            scope: DependencyScope::Compile,
         };
 
         assert_eq!(lib.group, "org.springframework.boot".to_string());
         assert_eq!(lib.name, "spring-boot-starter-web".to_string());
         assert_eq!(lib.version, "1.0.0-RELEASE".to_string());
-        assert_eq!(lib.scope, LibraryScope::Compile);
+        assert_eq!(lib.scope, DependencyScope::Compile);
     }
 }
