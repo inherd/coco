012bc5669de3cb04f54678bf6e8029c137d6d661
Phodal Huang <h@phodal.com>
2021-02-27 21:13:47 +0800

    feat(uml): make basic transform uml support

--- plugins/coco_struct_analysis/src/plantuml_render.rs
+++ plugins/coco_struct_analysis/src/plantuml_render.rs
@@ -7,8 +7,23 @@ pub trait PlantUml {
 pub struct PlantUmlRender;
 
 impl PlantUmlRender {
-    pub fn render(_classes: Vec<ClassInfo>) -> String {
-        let rendered: Vec<String> = vec![];
+    pub fn render(classes: Vec<ClassInfo>) -> String {
+        let mut rendered: Vec<String> = vec![];
+        for clazz in classes {
+            let mut members = vec![];
+            for member in clazz.members {
+                members.push(format!("  {}{}\n", member.access, member.name))
+            }
+            let mut methods = vec![];
+            let mut content = format!("{}", members.join(""));
+            for method in clazz.methods {
+                methods.push(format!("  {}{}()\n", method.access, method.name))
+            }
+            content = format!("{}{}", content, methods.join(""));
+
+            rendered.push(format!("class {{\n{}}}", content));
+        }
+
         let dep: Vec<String> = vec![];
 
         format!(
@@ -21,6 +36,7 @@ impl PlantUmlRender {
 
 #[cfg(test)]
 mod tests {
+    use crate::coco_struct::{ClassInfo, MemberInfo, MethodInfo};
     use crate::plantuml_render::PlantUmlRender;
 
     #[test]
@@ -28,5 +44,35 @@ mod tests {
         let classes = vec![];
         let str = PlantUmlRender::render(classes);
         assert_eq!("@startuml\n\n\n\n@enduml", str);
+    }
+
+    #[test]
+    fn should_render_single_empty_class() {
+        let mut classes = vec![];
+        let demo = ClassInfo::new("Demo");
+        classes.push(demo);
+
+        let str = PlantUmlRender::render(classes);
+        assert_eq!("@startuml\n\nclass {\n}\n\n@enduml", str);
+    }
+
+    #[test]
+    fn should_render_member_method() {
+        let mut classes = vec![];
+        let mut demo = ClassInfo::new("Demo");
+
+        let member = MemberInfo::new("demo", "-", "String".to_string());
+        demo.members.push(member);
+
+        let method = MethodInfo::new("method", "-", "String".to_string());
+        demo.methods.push(method);
+
+        classes.push(demo);
+
+        let str = PlantUmlRender::render(classes);
+        assert_eq!(
+            "@startuml\n\nclass {\n  -demo\n  -method()\n}\n\n@enduml",
+            str
+        );
     }
 }
