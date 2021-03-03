90964e32a286945fc63e6ff5fe739883a6ea2373
Phodal Huang <h@phodal.com>
2021-03-03 17:39:11 +0800

    feat(codeflower): add code flower drags

--- web/public/js/graph/cloc/code-flower.js
+++ web/public/js/graph/cloc/code-flower.js
@@ -16,8 +16,8 @@ function renderCodeFlower(data, selector) {
   let w = GraphConfig.width;
   let h = GraphConfig.height;
 
-  const links = data.links.map(d => Object.create(d));
-  const nodes = data.nodes.map(d => Object.create(d));
+  const links = data.links;
+  const nodes = data.nodes;
 
   d3.select(selector).selectAll("svg").remove();
   const svg = d3.select(selector).append("svg").attr("viewBox", [0, 0, w, h]);
@@ -48,19 +48,45 @@ function renderCodeFlower(data, selector) {
     .join("circle")
     .attr("r", 5)
     .attr("fill", color)
+    .call(drag(simulation));
 
   node.append("title")
     .text(d => d.id);
 
   simulation.on("tick", () => {
-    link
-      .attr("x1", d => d.source.x)
-      .attr("y1", d => d.source.y)
-      .attr("x2", d => d.target.x)
-      .attr("y2", d => d.target.y);
+      link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
-    node
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
-  });
+      node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+    });
+
+  function drag() {
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+
+    return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+
+  }
 }
