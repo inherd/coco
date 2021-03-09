3a8b39630664a0e494275b76b86d8a95696c41f5
ynfeng <fynwin@gmail.com>
2021-03-09 20:05:15 +0800

    rename XPathContext -> PomParser.

--- psa/src/jvm/maven_dependency.rs
+++ psa/src/jvm/maven_dependency.rs
@@ -9,25 +9,25 @@ pub struct MavenDependencyAnalyzer {
     pub properties: HashMap<String, String>,
 }
 
-struct XPathContext<'a> {
+struct PomParser<'a> {
     factory: Factory,
     context: Context<'a>,
     package: Package,
 }
 
-impl<'a> XPathContext<'a> {
-    fn new(xml_content: &str) -> XPathContext {
+impl<'a> PomParser<'a> {
+    fn new(xml_content: &str) -> PomParser {
         let mut context = Context::new();
         context.set_namespace("ns", "http://maven.apache.org/POM/4.0.0");
 
-        XPathContext {
+        PomParser {
             package: parser::parse(xml_content).unwrap(),
             factory: Factory::new(),
             context,
         }
     }
 
-    fn evaluate(&self, expression: &str) -> Value {
+    fn xpath_evaluate(&self, expression: &str) -> Value {
         let xpath = self.factory.build(expression).unwrap().unwrap();
         xpath
             .evaluate(&self.context, self.package.as_document().root())
@@ -36,14 +36,14 @@ impl<'a> XPathContext<'a> {
 
     fn parse_properties(&self) -> HashMap<String, String> {
         let mut properties = HashMap::with_capacity(10);
-        let num_of_properties = self.evaluate("count(/ns:project/ns:properties/*)");
+        let num_of_properties = self.xpath_evaluate("count(/ns:project/ns:properties/*)");
         if let Value::Number(ref count) = num_of_properties {
             for i in 0..(count.round() as i64) {
                 let prop_name_expression = format!("name(/ns:project/ns:properties/*[{}])", i + 1);
                 let prop_value_expression = format!("/ns:project/ns:properties/*[{}]", i + 1);
 
-                let prop_name = self.evaluate(prop_name_expression.as_str());
-                let prop_value = self.evaluate(prop_value_expression.as_str());
+                let prop_name = self.xpath_evaluate(prop_name_expression.as_str());
+                let prop_value = self.xpath_evaluate(prop_value_expression.as_str());
                 properties.insert(prop_name.string(), prop_value.string());
             }
         }
@@ -77,63 +77,63 @@ impl DependencyAnalyzer for MavenDependencyAnalyzer {
 
 fn parse_deps(xml_content: &str) -> Vec<Dependency> {
     let mut deps = vec![];
-    let xpath_context = XPathContext::new(xml_content);
+    let pom_parser = PomParser::new(xml_content);
 
-    let num_of_deps = xpath_context.evaluate("count(/ns:project/ns:dependencies/ns:dependency)");
+    let num_of_deps = pom_parser.xpath_evaluate("count(/ns:project/ns:dependencies/ns:dependency)");
     if let Value::Number(ref count) = num_of_deps {
         for i in 0..(count.round() as i64) {
             deps.push(Dependency {
-                group: parse_group_id(&xpath_context, i),
-                name: parse_artifact_id(&xpath_context, i),
-                version: parse_version(&xpath_context, i),
-                scope: parse_scope(&xpath_context, i),
+                group: parse_group_id(&pom_parser, i),
+                name: parse_artifact_id(&pom_parser, i),
+                version: parse_version(&pom_parser, i),
+                scope: parse_scope(&pom_parser, i),
             });
         }
     };
     deps
 }
 
-fn parse_group_id(xpath_context: &XPathContext, i: i64) -> String {
+fn parse_group_id(pom_parser: &PomParser, i: i64) -> String {
     let group_id_expression = format!(
         "/ns:project/ns:dependencies/ns:dependency[{}]/ns:groupId",
         i + 1
     );
-    xpath_context
-        .evaluate(group_id_expression.as_str())
+    pom_parser
+        .xpath_evaluate(group_id_expression.as_str())
         .string()
 }
 
-fn parse_artifact_id(xpath_context: &XPathContext, i: i64) -> String {
+fn parse_artifact_id(pom_parser: &PomParser, i: i64) -> String {
     let artifact_id_expression = format!(
         "/ns:project/ns:dependencies/ns:dependency[{}]/ns:artifactId",
         i + 1
     );
-    let name = xpath_context
-        .evaluate(artifact_id_expression.as_str())
+    let name = pom_parser
+        .xpath_evaluate(artifact_id_expression.as_str())
         .string();
     name
 }
 
-fn parse_version(xpath_context: &XPathContext, i: i64) -> String {
+fn parse_version(pom_parser: &PomParser, i: i64) -> String {
     let version_expression = format!(
         "/ns:project/ns:dependencies/ns:dependency[{}]/ns:version",
         i + 1
     );
-    let version = xpath_context.evaluate(version_expression.as_str()).string();
+    let version = pom_parser.xpath_evaluate(version_expression.as_str()).string();
     if version.starts_with("${") && version.ends_with("}") {
-        let properties = xpath_context.parse_properties();
+        let properties = pom_parser.parse_properties();
         let prop_name = &version[2..version.len() - 1];
         return properties.get(prop_name).unwrap().as_str().to_string();
     }
     version
 }
 
-fn parse_scope(xpath_context: &XPathContext, i: i64) -> DependencyScope {
+fn parse_scope(pom_parser: &PomParser, i: i64) -> DependencyScope {
     let scope_expression = format!(
         "/ns:project/ns:dependencies/ns:dependency[{}]/ns:scope",
         i + 1
     );
-    let scope_content = xpath_context.evaluate(scope_expression.as_str());
+    let scope_content = pom_parser.xpath_evaluate(scope_expression.as_str());
     let scope = match scope_content.string().as_str() {
         "test" => DependencyScope::Test,
         _ => DependencyScope::Compile,
