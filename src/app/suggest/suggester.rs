6a668e6d914dfcf9f6a7d650a040c46782d00558
Phodal Huang <h@phodal.com>
2021-03-31 07:43:42 +0800

    refactor: add handle for load struct

--- src/app/suggest/suggester.rs
+++ src/app/suggest/suggester.rs
@@ -7,16 +7,32 @@ pub struct Suggester;
 
 impl Suggester {
     pub fn run(project: String) {
-        let model = Suggester::load_struct(project);
-        let suggest: ModelSuggest = ModelSuggest::new(model);
-        suggest.analysis_all();
+        match Suggester::load_struct(project) {
+            Ok(model) => {
+                let suggest: ModelSuggest = ModelSuggest::new(model);
+                suggest.analysis_all();
+            }
+            Err(_) => {}
+        }
     }
 
-    fn load_struct(project: String) -> Vec<ClassInfo> {
+    fn load_struct(project: String) -> Result<Vec<ClassInfo>, String> {
         let file_name = format!("{}.json", project);
         let path = Settings::struct_dir().join(file_name);
-        let contents = fs::read_to_string(path).expect("lost path");
-        let model: Vec<ClassInfo> = serde_json::from_str(contents.as_str()).expect("error format");
-        model
+        let contents;
+        match fs::read_to_string(path) {
+            Ok(str) => contents = str,
+            Err(error) => {
+                return Err(format!("{}", error));
+            }
+        }
+        let model: Vec<ClassInfo>;
+        match serde_json::from_str(contents.as_str()) {
+            Ok(m) => model = m,
+            Err(error) => {
+                return Err(format!("{}", error));
+            }
+        }
+        return Ok(model);
     }
 }
