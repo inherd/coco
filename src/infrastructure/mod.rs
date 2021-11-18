005d6dc95433928d83eee70e35932732d33ddf16
Phodal Huang <h@phodal.com>
2021-11-18 19:07:32 +0800

    refactor: clean code

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,7 +1,6 @@
 pub mod cloc;
 pub mod file_scanner;
 pub mod git;
-pub mod include_parser;
 pub mod time_format;
 
 pub use translator::Translator;
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ extern crate lazy_static;
 extern crate serde;
 
 extern crate pest;
-#[macro_use]
 extern crate pest_derive;
 
 extern crate dlopen;
